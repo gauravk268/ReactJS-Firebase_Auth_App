43bef6b03224bcfdd6944ca3cccf1c1ac25be681
Gaurav Kumar <gauravk26800@gmail.com>
2021-03-28 14:20:52 +0530

    added signup element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
+import { Signup } from "./components";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello World!!</h1>
+        <Signup />
       </header>
     </div>
   );
